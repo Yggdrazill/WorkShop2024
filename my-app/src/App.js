19139a4a0858096052bbc8d2d0d9c20d934aea11
Andreas Bäärnhielm <andreas.baarnhielm@trimma.se>
2022-11-15 14:28:59 +0100

    Städat upp utseendet lite

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -73,26 +73,35 @@ export default function App(props) {
 		fetchOrders();
 	}, []);
 
+	const IconTrashCan = () => (
+		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+			<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+			<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+		</svg>
+	);
+
 	const ShoppingApp = () => {
 		return (
 			<>
 				<h3>
 					Varor
 				</h3>
-				{
-					items?.map(i => {
-						return (
-							<div key={i.id}>
-								<Item
-									id={i.id}
-									imageId={i.imageId}
-									name={i.name}
-									cost={i.cost}
-									onBuy={() => handleBuy(i, 1)}
-								/>
-							</div>
-					)})
-				}
+				<div className="itemListWrapper">
+					<div className="itemList">
+						{items?.map(i => {
+							return (
+								<div key={i.id}>
+									<Item
+										id={i.id}
+										imageId={i.imageId}
+										name={i.name}
+										cost={i.cost}
+										onBuy={() => handleBuy(i, 1)}
+									/>
+								</div>
+						)})}
+					</div>
+				</div>
 				<h3>
 					Varukorg 
 					<input value="Töm" type="button" onClick={clearCart} />
@@ -102,16 +111,19 @@ export default function App(props) {
 						Totalt: {totalPrice} kr
 					</b>
 				</p>
-				<div>
-					{orders?.map((order, index) => (
-						<div key={index}>
-							<p>
-								{order.item.name} x {order.quantity} - {order.item.cost * order.quantity}
-								<span style={{ marginLeft: 10, cursor: 'pointer' }} onClick={() => handleRemove(order.id)}>X</span>
-							</p>
-						</div>
-					))}
+				<div className="orderListWrapper">
+					<div className="orderList">
+						{orders?.map((order, index) => (
+							<div key={index}>
+								<p className={"order"}>
+									{order.item.name} x {order.quantity} - {order.item.cost * order.quantity}
+									<span style={{ marginLeft: 10, cursor: 'pointer' }} onClick={() => handleRemove(order.id)}>{IconTrashCan()}</span>
+								</p>
+							</div>
+						))}
+					</div>
 				</div>
+				
 			</>
 		);
 	}
@@ -122,7 +134,8 @@ export default function App(props) {
 				<h3>
 					Varor
 				</h3>
-				{items?.map(i => {
+				<div className="itemList">
+					{items?.map(i => {
 					return (
 						<div key={i.id}>
 							<Item
@@ -134,6 +147,8 @@ export default function App(props) {
 							/>
 						</div>
 				)})}
+				</div>
+				
 			</>
 		);
 	}
